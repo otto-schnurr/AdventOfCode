76a84cae1f671c8f3e32d63431eda90dba16b0a7
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-12 21:56:09 -0500

    Add expected output to solution tests.

--- Solutions/test.sh
+++ Solutions/test.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-echo "hello world"
+function test_solution()
+{
+    local PROBLEM=${1%*.swift}
+    PROBLEM=${PROBLEM#day*}
+
+    echo -n "day $PROBLEM: "
+    $1 < input${PROBLEM}.txt > test.txt
+
+     if [ -e test.txt ] && { diff expected-output${PROBLEM}.txt test.txt; }
+    then
+        echo "passed"
+    fi
+
+    rm -f test.txt
+}
+
+for solution in *day*.swift
+do
+    test_solution $solution
+done

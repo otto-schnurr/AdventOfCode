e89130c70ed79ae30f9e8733a78ff95806eda6cc
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-12 21:54:16 -0500

    fix: Solution for day 4.

--- Solutions/day04.swift
+++ Solutions/day04.swift
@@ -1,51 +1,68 @@
 #!/usr/bin/env swift
 
+struct Digits: Sequence, IteratorProtocol {
+    
+    init(_ value: Int) { remainingDigits = value }
+    
+    mutating func next() -> Int? {
+        guard remainingDigits > 0 else { return nil }
+        defer { remainingDigits /= 10 }
+        return remainingDigits % 10
+    }
+    
+    private var remainingDigits: Int
+    
+}
+
+struct DigitPairs: Sequence, IteratorProtocol {
+    
+    init(_ value: Int) { remainingDigits = value }
+    
+    mutating func next() -> (Int, Int)? {
+        guard remainingDigits >= 10 else { return nil }
+        let digits = remainingDigits % 100
+        remainingDigits /= 10
+        return (digits / 10, digits % 10)
+    }
+    
+    private var remainingDigits: Int
+    
+}
+
 extension Int {
 
-    var isValidPassword: Bool {
-        var remainingDigits = self
-        var hasAdjacentDigits = false
-
-        while let (firstDigit, secondDigit) = remainingDigits.lastTwoDigits {
-            remainingDigits /= 10
-            guard firstDigit <= secondDigit else { return false }
-            hasAdjacentDigits = hasAdjacentDigits || (firstDigit == secondDigit)
-        }
-
-        return hasAdjacentDigits
-    }
-
     var hasIncreasingDigits: Bool {
-        var remainingDigits = self
-
-        while let (firstDigit, secondDigit) = remainingDigits.lastTwoDigits {
-            remainingDigits /= 10
-            guard firstDigit <= secondDigit else { return false }
-        }
-
-        return true
+        DigitPairs(self).allSatisfy { $0 <= $1 }
     }
 
-    var hasAdjacenDigits: Bool {
-        var result = false
-
-        while let (firstDigit, secondDigit) = remainingDigits.lastTwoDigits {
-            remainingDigits /= 10
-            result = result || firstDigit == secondDigit
-        }
-
-        return result
+    var hasAtleastTwoAdjacentDigits: Bool {
+        DigitPairs(self).contains { $0 == $1 }
     }
-
-    var lastTwoDigits: (Int, Int)? {
-        guard self > 9 else { return nil }
-        let digits = self % 100
-        return (digits / 10, digits % 10)
+    
+    var hasTwoAdjacentDigits: Bool {
+        var previousDigit: Int?
+        var adjacentCount = 1
+        
+        for nextDigit in Digits(self) {
+            defer { previousDigit = nextDigit }
+            guard nextDigit == previousDigit else {
+                if adjacentCount == 2 { return true }
+                adjacentCount = 1
+                continue
+            }
+            
+            adjacentCount += 1
+        }
+        
+        return adjacentCount == 2
     }
 
 }
 
 let numbers = readLine()!.split(separator: "-").map { Int($0)! }
-let range = numbers[0] ... numbers[1]
+let passwords = (numbers[0] ... numbers[1]).filter { 
+    $0.hasIncreasingDigits && $0.hasAtleastTwoAdjacentDigits 
+}
 
-print("part 1: \(range.filter { $0.isValidPassword }.count)")
+print("part 1: \(passwords.count)")
+print("part 2: \(passwords.filter { $0.hasTwoAdjacentDigits }.count)")

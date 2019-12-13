e0e2c86ce571bbf4f68e8ce65c9f91502faea210
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-13 16:54:34 -0500

    Add ParameterMode unit tests.

--- Sources/ParameterMode.swift
+++ Sources/ParameterMode.swift
@@ -10,13 +10,24 @@ enum ParameterMode: Int {
 
     case position = 0
     case immediate = 1
-
-    static func parse(count: Int, from value: Int) -> [ParameterMode] {
+    
+    static func parse(count: Int, from word: Word) -> [ParameterMode] {
+        guard count > 0 && word > 0 else { return [] }
+        
         let modes = [100, 1_000, 10_000].map { (factor) -> ParameterMode in
-            let digit = value % (factor * 10) / factor
+            let digit = word % (factor * 10) / factor
             return ParameterMode(rawValue: digit)!
         }
         return Array(modes.prefix(count))
     }
 
+}
+
+extension ParameterMode: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .position:  return "position"
+        case .immediate: return "immediate"
+        }
+    }
 }

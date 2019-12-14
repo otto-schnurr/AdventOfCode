8dc6ae6da61916d1204dfee7a69729bf087da680
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-14 13:57:10 -0500

    Integrate permutations into day 7 solution.

--- Solutions/day07.swift
+++ Solutions/day07.swift
@@ -2,54 +2,6 @@
 
 import AdventOfCode // ..
 
-extension Array where Element == Int {
-    mutating func incrementBase5() -> Bool {
-        guard !isEmpty else { return false }
-
-        self[count - 1] += 1
-
-        (1 ..< count).reversed().forEach { index in
-            self[index - 1] += self[index] / 5
-            self[index] %= 5
-        }
-
-        return self[0] < 5
-    }
-}
-
-var permutations = [[Int]]()
-
-func permuteWirth(_ a: [Int], _ n: Int) {
-    if n == 0 {
-        permutations.append(a)
-    } else {
-        var a = a
-        permuteWirth(a, n - 1)
-        for i in 0..<n {
-            a.swapAt(i, n)
-            permuteWirth(a, n - 1)
-            a.swapAt(i, n)
-        }
-    }
-}
-
-permuteWirth([0, 1, 2, 3, 4], 4)
-
-struct PhaseCombinations: Sequence, IteratorProtocol {
-
-    var combination = [0, 0, 0, 0, 0]
-
-    mutating func next() -> [Int]? {
-        let result = combination
-        guard !result.isEmpty else { return nil }
-
-        if !combination.incrementBase5() { combination = [] }
-
-        return result
-    }
-
-}
-
 let original = readLine()!.split(separator: ",").map { Int($0)! }
 
 func runAmplifier(phase: Int, source: Int) -> Int {
@@ -63,4 +15,4 @@ func run(phases: [Int]) -> Int {
     return phases.reduce(0) { runAmplifier(phase: $1, source: $0) }
 }
 
-print(permutations.map(run).reduce(0,max))
+print(Array(0..<5).permutations.map(run).reduce(0, max))

b33e9db63220b0aef39d04e0b97d89c7fcb62fa5
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-14 13:55:07 -0500

    Remaining implementation for array.permutations.

--- Sources/Array+Permutations.swift
+++ Sources/Array+Permutations.swift
@@ -10,7 +10,26 @@ public extension Array {
     var permutations: [[Element]] {
         guard !isEmpty else { return [ ] }
         
-        // !!!: implement me
-        return [self]
+        var result = [[Element]]()
+        permuteWirth(self, count - 1, result: &result)
+        return result
+    }
+}
+
+
+// MARK: - Private
+
+// reference https://github.com/raywenderlich/swift-algorithm-club/tree/master/Combinatorics
+func permuteWirth<T>(_ a: [T], _ n: Int, result: inout [[T]]) {
+    if n == 0 {
+        result.append(a)
+    } else {
+        var a = a
+        permuteWirth(a, n - 1, result: &result)
+        for i in 0..<n {
+            a.swapAt(i, n)
+            permuteWirth(a, n - 1, result: &result)
+            a.swapAt(i, n)
+        }
     }
 }
--- Tests/Array+Permutations_tests.swift
+++ Tests/Array+Permutations_tests.swift
@@ -14,8 +14,33 @@ class Array_Permutations_tests: XCTestCase {
         XCTAssertEqual([Int]().permutations, [[Int]]())
     }
     
-    func test_permutationsOfArrayWithSingleElement_isItself() {
+    func test_permutationsOfSingleElementArray_isItself() {
         XCTAssertEqual(["foo"].permutations, [["foo"]])
     }
     
+    func test_permutationsOfTwoElementArray_hasExpectedResults() {
+        XCTAssertEqual(
+            ["foo", "bar"].permutations,
+            [["foo", "bar"], ["bar", "foo"]]
+        )
+    }
+
+    func test_permutationsOfThreeElementArray_hasExpectedResults() {
+        XCTAssertEqual(
+            ["foo", "bar", "dog"].permutations,
+            [
+                ["foo", "bar", "dog"],
+                ["bar", "foo", "dog"],
+                ["dog", "bar", "foo"],
+                ["bar", "dog", "foo"],
+                ["foo", "dog", "bar"],
+                ["dog", "foo", "bar"]
+            ]
+        )
+    }
+    
+    func test_fiveElementArray_hasExpectedNumberOfPermutations() {
+        XCTAssertEqual(Array((1...5)).permutations.count, 120)
+    }
+
 }

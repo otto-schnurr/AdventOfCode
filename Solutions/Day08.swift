89e3a0c2e98c2a55f5b79f658625cb68398e3292
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-15 15:48:54 -0500

    Add preliminary solution for day 8.
    Not yet implemented.

---
+++ Solutions/Day08.swift
@@ -0,0 +1,23 @@
+//
+//  Day08.swift
+//  AdventOfCode/Solutions
+//  
+//
+//  Created by Otto Schnurr on 12/15/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+
+class Day08: XCTestCase {
+    
+    func test_example() {
+    }
+    
+}
+
+
+// MARK: - Private
+private let _pixels = [
+    0
+]

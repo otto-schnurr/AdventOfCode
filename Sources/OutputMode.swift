dfd44c8e7d042e8bb6d6a27f1ac666d6183023cd
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-15 15:05:37 -0500

    Implement OutputMode.
    Not yet integrated.

--- Sources/OutputMode.swift
+++ Sources/OutputMode.swift
@@ -5,4 +5,10 @@
 //  Created by Otto Schnurr on 12/15/2019.
 //
 
-enum OutputMode { }
+enum OutputMode: String {
+    /// Execution continues after an output is generated.
+    case `continue`
+    
+    /// Execution stops after an output is generated but can be continued later.
+    case yield
+}

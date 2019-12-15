3a80ce960b8a4627473eb7bc4db87d551ea0fed2
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-15 10:36:55 -0500

    Add preliminary Day01 solution.
    Not yet implemented.

---
+++ Solutions/Day01.swift
@@ -0,0 +1,14 @@
+//
+//  Day01.swift
+//  AdventOfCode/Solutions
+//  
+//
+//  Created by Otto Schnurr on 12/15/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+
+class Day01: XCTestCase {
+    
+}

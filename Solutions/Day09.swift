cbfd27426b08c11c269d39a8f90c881553a9599d
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-15 23:12:08 -0500

    Add preliminary solution for day 9.
    Not yet implemented.

---
+++ Solutions/Day09.swift
@@ -0,0 +1,28 @@
+//
+//  Day09.swift
+//  AdventOfCode/Solutions
+//  
+//
+//  Created by Otto Schnurr on 12/15/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+import AdventOfCode
+
+class Day09: XCTestCase {
+    
+    func test_example() {
+        let _ = [
+            109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101,
+            1006, 101, 0, 99
+        ]
+    }
+    
+}
+
+
+// MARK: - Private
+private let _program = [
+    0
+]

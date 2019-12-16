eba50d129f81093a003124b35808832b9f23200c
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-16 00:59:51 -0500

    Solve first example for day 16.

--- Solutions/Day16.swift
+++ Solutions/Day16.swift
@@ -11,6 +11,12 @@ import XCTest
 class Day16: XCTestCase {
     
     func test_example() {
+        let data = "12345678".data(using: .utf8)!
+        let signal = data.map { Int($0) - 48 }
+        XCTAssertEqual(
+            signal.convolved(phaseCount: 4),
+            [0, 1, 0, 2, 9, 4, 9, 8]
+        )
     }
 
 }
@@ -21,3 +27,44 @@ private let _input: Data = {
     let resourceURL = URL(testHarnessResource: "input10.txt")
     return try! Data(contentsOf: resourceURL)
 }()
+
+private let _basePattern = [0, 1, 0, -1]
+
+private struct Pattern: Sequence, IteratorProtocol {
+    
+    let order: Int
+    var index = 1
+    
+    init(order: Int) { self.order = order }
+    
+    mutating func next() -> Int? {
+        let patternIndex = (index / order) % _basePattern.count
+        index += 1
+        return _basePattern[patternIndex]
+    }
+
+}
+
+private extension Array where Element == Int {
+    
+    func convolved(phaseCount: Int) -> Self {
+        var result = self
+        
+        for _ in 1...phaseCount {
+            result = (1...result.count).map { result.convolved(order: $0) }
+        }
+        
+        return result
+    }
+    
+    func convolved(order: Int) -> Element {
+        let pattern = Pattern(order: order)
+        let accumulation = zip(self, pattern).reduce(0) { result, entry in
+            print("\(entry.0)*\(entry.1) + ", terminator: "")
+            return result + entry.0 * entry.1
+        }
+        print("= \(abs(accumulation) % 10)")
+        return abs(accumulation) % 10
+    }
+    
+}

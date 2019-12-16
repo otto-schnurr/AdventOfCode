5fc6d305736497c603a31b84128889dc25b9087d
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-16 00:16:19 -0500

    Add preliminary solution for day 16.
    Not yet implemented.

---
+++ Solutions/Day16.swift
@@ -0,0 +1,23 @@
+//
+//  Day16.swift
+//  AdventOfCode/Solutions
+//
+//  Created by Otto Schnurr on 12/16/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+
+class Day16: XCTestCase {
+    
+    func test_example() {
+    }
+
+}
+
+
+// MARK: - Private
+private let _input: Data = {
+    let resourceURL = URL(testHarnessResource: "input10.txt")
+    return try! Data(contentsOf: resourceURL)
+}()

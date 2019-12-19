f14ba3a4052c5fb4d7c1ae104bd1f4c825f3ca5c
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-18 21:02:07 -0500

    day 10: Add preliminary solution for examples.
    Not yet implemented.

---
+++ Solutions/Day10.swift
@@ -0,0 +1,17 @@
+//
+//  Day10.swift
+//  AdventOfCode/Solutions
+//
+//  Created by Otto Schnurr on 12/18/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+
+class Day10: XCTestCase {
+    
+    func test_examples() {
+        
+    }
+    
+}

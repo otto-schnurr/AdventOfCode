8ad7d9de9b414cba153d28a1076136abae3bae1f
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-19 20:15:36 -0500

    day 10: Add coordinate.reduced.
    In anticipation of dividing vectors into smaller integral steps.

--- Solutions/Day10.swift
+++ Solutions/Day10.swift
@@ -23,18 +23,54 @@ class Day10: XCTestCase {
         )
     }
     
+    func test_reduceCoordinate() {
+        XCTAssertEqual(Coordinate.zero.reduced, .zero)
+        
+        XCTAssertEqual(Coordinate(1, 0).reduced, Coordinate(1, 0))
+        XCTAssertEqual(Coordinate(2, 0).reduced, Coordinate(1, 0))
+        XCTAssertEqual(Coordinate(5, 0).reduced, Coordinate(1, 0))
+        
+        XCTAssertEqual(Coordinate(0, 1).reduced, Coordinate(0, 1))
+        XCTAssertEqual(Coordinate(0, 2).reduced, Coordinate(0, 1))
+        XCTAssertEqual(Coordinate(0, 5).reduced, Coordinate(0, 1))
+        
+        XCTAssertEqual(Coordinate(1, 1).reduced, Coordinate(1, 1))
+        XCTAssertEqual(Coordinate(2, 2).reduced, Coordinate(1, 1))
+        XCTAssertEqual(Coordinate(5, 5).reduced, Coordinate(1, 1))
+
+        XCTAssertEqual(Coordinate(1, 2).reduced, Coordinate(1, 2))
+        XCTAssertEqual(Coordinate(2, 4).reduced, Coordinate(1, 2))
+        XCTAssertEqual(Coordinate(5, 10).reduced, Coordinate(1, 2))
+
+        XCTAssertEqual(Coordinate(-5, 0).reduced, Coordinate(-1, 0))
+        XCTAssertEqual(Coordinate(0, -5).reduced, Coordinate(0, -1))
+        XCTAssertEqual(Coordinate(-5, 5).reduced, Coordinate(-1, 1))
+        XCTAssertEqual(Coordinate(5, -5).reduced, Coordinate(1, -1))
+        XCTAssertEqual(Coordinate(-5, -5).reduced, Coordinate(-1, -1))
+    }
+    
 }
 
 
 // MARK: - Private
 private struct Coordinate: Equatable {
+
+    static let zero = Coordinate(0, 0)
+
     let x: Int
     let y: Int
     
+    var reduced: Coordinate {
+        guard self != .zero else { return self }
+        let divisor = gcd(abs(x), abs(y))
+        return Coordinate(x/divisor, y/divisor)
+    }
+
     init(_ x: Int, _ y: Int) {
         self.x = x
         self.y = y
     }
+
 }
 
 extension Coordinate: CustomStringConvertible {
@@ -51,4 +87,18 @@ private func parseCoordinates(from map: String) -> [Coordinate] {
     }
     
     return result
+}
+
+// reference: https://github.com/raywenderlich/swift-algorithm-club
+private func gcd(_ m: Int, _ n: Int) -> Int {
+    var a: Int = 0
+    var b: Int = max(m, n)
+    var r: Int = min(m, n)
+    
+    while r != 0 {
+        a = b
+        b = r
+        r = a % b
+    }
+    return b
 }

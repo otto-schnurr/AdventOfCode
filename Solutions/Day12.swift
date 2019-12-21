49416a9436a5e55726c481834bd1d65aa40cfc96
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-20 21:31:23 -0500

    day 12: Add preliminary solution.
    Not yet implemented.

---
+++ Solutions/Day12.swift
@@ -0,0 +1,17 @@
+//
+//  Day12.swift
+//  AdventOfCode/Solutions
+//
+//  Created by Otto Schnurr on 12/20/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+
+class Day12: XCTestCase {
+
+    func test_example() {
+        
+    }
+    
+}

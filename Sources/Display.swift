02e06927362d6c819e8909ba689989a256fd50d4
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-22 12:02:22 -0500

    Implement private Screen for Display.
    Not yet integrated.

--- Sources/Display.swift
+++ Sources/Display.swift
@@ -39,3 +39,44 @@ public struct Display {
     
 }
 
+
+// MARK: Private
+private struct Screen {
+    
+    let width: Int
+    let height: Int
+    
+    subscript(coordinate: Coordinate) -> Display.Pixel {
+        get { pixels[coordinate.y][coordinate.x] }
+        set(newValue) { pixels[coordinate.y][coordinate.x] = newValue }
+    }
+
+    init?(width: Int, height: Int, initialColor: Display.Pixel) {
+        guard width > 0 && height > 0 else { return nil }
+
+        self.width = width
+        self.height = height
+        let row = Array(repeating: initialColor, count: width)
+        pixels = Array(repeating: row, count: height)
+    }
+    
+    // MARK: Private
+    private var pixels: [[Display.Pixel]]
+    
+}
+
+private extension Screen {
+    
+    init?(pixels: [Coordinate: Display.Pixel], backgroundColor: Character) {
+        let width = pixels.reduce(0) { max($0, $1.key.x) }
+        let height = pixels.reduce(0) { max($0, $1.key.y) }
+        guard
+            var screen = Screen(
+                width: width, height: height, initialColor: backgroundColor
+        ) else { return nil }
+        
+        pixels.forEach { screen[$0] = $1 }
+        self = screen
+    }
+    
+}

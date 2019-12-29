2275eaad76932d23a7d1f0df698ff3b8a72d7470
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-28 19:58:11 -0500

    day 17: Add preliminary solution.
    Not yet implemented.

---
+++ Solutions/Day17.swift
@@ -0,0 +1,22 @@
+//
+//  Day17.swift
+//  AdventOfCode/Solutions
+//
+//  Created by Otto Schnurr on 12/28/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+import AdventOfCode
+
+final class Day17: XCTestCase {
+    
+    func test_solutions() {
+        XCTAssertFalse(_program.isEmpty)
+    }
+    
+}
+
+
+// MARK: - Private
+private let _program = Program(testHarnessResource: "input17.txt")!

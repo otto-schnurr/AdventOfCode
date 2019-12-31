bcfd39bb43847b0a6b89f56b9ca5fe6de542aeb2
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-31 14:31:10 -0500

    day 17: Print a turn-by-turn list of directions.

--- Solutions/Day17.swift
+++ Solutions/Day17.swift
@@ -24,6 +24,12 @@ final class Day17: XCTestCase {
             screen.intersections(of: "#").reduce(0) { $0 + $1.x * $1.y },
             9876
         )
+        
+        let startingPosition = screen.firstCoordinate(of: "^")!
+        let commands = screen.trace(
+            "#", from: startingPosition, startingDirection: .north
+        )
+        print(commands)
     }
     
 }
@@ -32,7 +38,32 @@ final class Day17: XCTestCase {
 // MARK: - Private
 private let _program = Program(testHarnessResource: "input17.txt")!
 
+private struct Command {
+    
+    let turn: Direction.Turn
+    let distance: Int
+    
+    static func +(command: Command, additionalDistance: Int) -> Command {
+        return Command(
+            turn: command.turn, distance: command.distance + additionalDistance
+        )
+    }
+    
+}
+
+extension Command: CustomStringConvertible {
+    var description: String {
+        let turnDescription: String
+        switch turn {
+        case .left:  turnDescription = "L"
+        case .right: turnDescription = "R"
+        }
+        return "\(turnDescription)\(distance)"
+    }
+}
+
 private extension Screen {
+    
     func intersections(of pixel: Pixel) -> [Coordinate] {
         guard width >= 5 && height >= 5 else { return [ ] }
 
@@ -52,5 +83,53 @@ private extension Screen {
         }
         
         return result
+    }
+    
+    func trace(
+        _ path: Pixel, from startingPosition: Coordinate, startingDirection: Direction
+    ) -> [Command] {
+        var position = startingPosition
+        var direction = startingDirection
+        var result = [Command]()
+        
+        while let nextTurn = nextTurn(
+            along: path, at: position, heading: direction
+        ) {
+            direction = direction.turned(nextTurn)
+            var command = Command(turn: nextTurn, distance: 0)
+
+            while
+                validate(coordinate: position + direction) &&
+                self[position + direction] == path {
+                    
+                position = position + direction
+                command = command + 1
+            }
+            
+            if command.distance > 0 { result.append(command) }
+        }
+        
+        return result
+    }
+    
+    func nextTurn(
+        along path: Pixel, at position: Coordinate, heading direction: Direction
+    ) -> Direction.Turn? {
+        return Direction.Turn.all.first { turn in
+            let newPosition = position + direction.turned(turn)
+            return validate(coordinate: newPosition) && self[newPosition] == path
+        }
+    }
+    
+}
+
+private extension Direction {
+    static func nextDirections(afterStopping direction: Self) -> [Self] {
+        switch direction {
+        case .north: return [.west, .east]
+        case .south: return [.east, .west]
+        case .west:  return [.south, .north]
+        case .east:  return [.north, .south]
+        }
     }
 }

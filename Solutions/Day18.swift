052bb32e161311abddb03db0172b968e7313f8c9
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-31 19:32:51 -0500

    day 18: Add preliminary solution.
    Not yet implemented.

---
+++ Solutions/Day18.swift
@@ -0,0 +1,15 @@
+//
+//  Day18.swift
+//  AdventOfCode/Solutions
+//
+//  Created by Otto Schnurr on 12/31/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+import XCTest
+
+final class Day18: XCTestCase {
+}
+
+
+// MARK: - Private

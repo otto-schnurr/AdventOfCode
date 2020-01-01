fff655db143d86092742c24548e0c17c8942a3ec
Otto Schnurr <otto.schnurr@gmail.com>
2019-12-31 19:57:58 -0500

    Add preliminary search extension for screens.
    Not yet implemented.

---
+++ Sources/Screen+Search.swift
@@ -0,0 +1,11 @@
+//
+//  Screen+Search.swift
+//  AdventOfCode
+//
+//  Created by Otto Schnurr on 12/31/2019.
+//  Copyright © 2019 Otto Schnurr. All rights reserved.
+//
+
+public extension Screen {
+    
+}

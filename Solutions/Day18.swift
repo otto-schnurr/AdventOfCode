28068f57541e4c299275e0ce747fee35d5398c09
Otto Schnurr <otto.schnurr@gmail.com>
2020-01-01 10:29:34 -0500

    day 18: Define terrain type.
    Not yet integrated.

--- Solutions/Day18.swift
+++ Solutions/Day18.swift
@@ -24,3 +24,41 @@ private var _map: Screen = {
     let pixels = try! TestHarnessInput("input18.txt").map({ Array($0) })
     return Screen(pixels: pixels)!
 }()
+
+private enum Terrain {
+    
+    case start
+    case wall
+    case path
+    case key(Screen.Pixel)
+    case door(Screen.Pixel)
+    
+    init?(pixelValue: Screen.Pixel) {
+        switch pixelValue {
+        case "@": self = .start
+        case "#": self = .wall
+        case ".": self = .path
+        case "a"..."z": self = .key(pixelValue)
+        case "A"..."Z": self = .door(pixelValue)
+        default: return nil
+        }
+    }
+    
+}
+
+private extension Terrain {
+    var pixelValue: Screen.Pixel {
+        switch self {
+        case .start:
+            return "@"
+        case .wall:
+            return "#"
+        case .path:
+            return "."
+        case .key(let pixelValue):
+            return pixelValue
+        case .door(let pixelValue):
+            return pixelValue
+        }
+    }
+}

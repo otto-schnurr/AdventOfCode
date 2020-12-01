9ccabfb296556becfd78e4fa3c3c4c17ebca4fd2
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-01 09:01:20 -0500

    day 1, 2020: Reduce computation by a factor of 50.

--- 2020/Solutions/Day01.swift
+++ 2020/Solutions/Day01.swift
@@ -16,7 +16,7 @@ import XCTest
 final class Day01: XCTestCase {
 
     func test_example() {
-        let expenses = [1721, 979, 366, 299, 675, 1456]
+        var expenses = [1721, 979, 366, 299, 675, 1456]
         
         var combination = expenses.firstCombination(ofCount: 2, summingTo: 2020)!
         XCTAssertEqual(combination[0] * combination[1], 514579)
@@ -26,7 +26,7 @@ final class Day01: XCTestCase {
     }
     
     func test_solution() {
-        let expenses = Array(Input())
+        var expenses = Array(Input())
         
         var combination = expenses.firstCombination(ofCount: 2, summingTo: 2020)!
         XCTAssertEqual(combination[0] * combination[1], 858496)
@@ -51,8 +51,27 @@ private struct Input: Sequence, IteratorProtocol {
 }
 
 private extension Array where Element == Int {
-    func firstCombination(ofCount count: Int, summingTo sum: Int) -> [Int]? {
-        let combinations = self.combinations(ofCount: count)
-        return combinations.first() { $0.reduce(0, +) == sum }
+    mutating func firstCombination(ofCount count: Int, summingTo sum: Int) -> [Int]? {
+        // optimization: Values above this threshold are too big to combine with each other.
+        let index = partition { $0 > sum / 2 }
+        let smallValues = Array(self[0 ..< index])
+        let smallCombinations = smallValues.combinations(ofCount: count)
+
+        if let result = smallCombinations.first(summingTo: sum) { return result }
+
+        for largeValue in self[index ..< endIndex] {
+            let collection = smallValues + [largeValue]
+            let combinations = collection.combinations(ofCount: count)
+            
+            if let result = combinations.first(summingTo: sum) { return result }
+        }
+
+        return nil
+    }
+}
+
+private extension Combinations where Base == [Int] {
+    func first(summingTo sum: Int) -> Element? {
+        return first { $0.reduce(0, +) == sum }
     }
 }

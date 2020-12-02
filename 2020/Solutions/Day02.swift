5f2021f4d964bbbba399bd8cdb61d81daa296dd9
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-02 06:26:08 -0500

    day 2, 2020: Preliminary solution.
    Not yet implemented.

---
+++ 2020/Solutions/Day02.swift
@@ -0,0 +1,29 @@
+//  MIT License
+//  Copyright © 2020 Otto Schnurr
+//  https://github.com/otto-schnurr/AdventOfCode/blob/master/LICENSE
+
+//
+//  Day02.swift
+//  AdventOfCode-Solutions
+//
+//  A solution for https://adventofcode.com/2020/day/2
+//  Created by Otto Schnurr on 12/2/2020.
+//
+
+import Algorithms
+import XCTest
+
+final class Day02: XCTestCase {
+
+    func test_example() {
+        _ = [
+            "1-3 a: abcde",
+            "1-3 b: cdefg",
+            "2-9 c: ccccccccc"
+        ]
+    }
+    
+}
+
+
+// MARK: - Private

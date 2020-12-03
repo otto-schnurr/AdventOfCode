c51e0ff94daeaa6400c5144a7a531a97d3ee4e72
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-03 07:58:41 -0500

    day 3, 2020: Solve part 1 example.
    Anticipating, SIMD2<Int32> used by path finding API in Gameplay Kit.

--- 2020/Solutions/Day03.swift
+++ 2020/Solutions/Day03.swift
@@ -15,6 +15,25 @@ import XCTest
 final class Day03: XCTestCase {
 
     func test_example() {
+        let map = """
+        ..##.......
+        #...#...#..
+        .#....#..#.
+        ..#.#...#.#
+        .#...##..#.
+        ..#.##.....
+        .#.#.#....#
+        .#........#
+        #.##...#...
+        #...##....#
+        .#..#...#.#
+        """
+        let treePositions = [Position](from: map)
+        let mapWidth = map.prefix { $0 != Character("\n") }.count
+        let collisionCount = treePositions.count(
+            from: .zero, by: Position(3, 1), wrappingWidthBy: mapWidth
+        )
+        XCTAssertEqual(collisionCount, 7)
     }
     
     func test_solution() {
@@ -24,3 +43,39 @@ final class Day03: XCTestCase {
 
 
 // MARK: - Private
+private typealias Position = SIMD2<Int32>
+
+private extension Position {
+    init(_ x: Int, _ y: Int) {
+        self.init(Scalar(x), Scalar(y))
+    }
+}
+
+extension Array where Element == Position {
+    
+    init(from map: String) {
+        var result = [Position]()
+
+        for (y, row) in map.split(separator: "\n").enumerated() {
+            for (x, _) in row.enumerated().filter({ $0.element == "#" }) {
+                result.append(Position(x, y))
+            }
+        }
+        
+        self = result
+    }
+    
+    func count(from start: Position, by stride: Position, wrappingWidthBy width: Int) -> Int {
+        var position = start
+        var result = 0
+        
+        while position.y < count {
+            let wrappedPosition = Position(Int(position.x) % width, Int(position.y))
+            if self.contains(wrappedPosition) { result += 1 }
+            position &+= stride
+        }
+        
+        return result
+    }
+    
+}

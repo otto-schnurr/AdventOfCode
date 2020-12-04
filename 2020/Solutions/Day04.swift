bc21a4397b8e45ead1a92d96e01e8b41fc6cdf2d
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-04 07:41:26 -0500

    day 4, 2020: Add preliminary solution for example.
    Not yet solved.

--- 2020/Solutions/Day04.swift
+++ 2020/Solutions/Day04.swift
@@ -14,7 +14,24 @@ import XCTest
 
 final class Day04: XCTestCase {
 
-    func test_example() {
+    func DISABLED_test_example() {
+        let data = """
+        ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+        byr:1937 iyr:2017 cid:147 hgt:183cm
+
+        iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+        hcl:#cfa07d byr:1929
+
+        hcl:#ae17e1 iyr:2013
+        eyr:2024
+        ecl:brn pid:760753108 byr:1931
+        hgt:179cm
+
+        hcl:#cfa07d eyr:2025 pid:166559648
+        iyr:2011 ecl:brn hgt:59in
+        """
+        let lines = _scan(data)
+        XCTAssertEqual(_validCount(for: lines), 2)
     }
     
     func test_solution() {
@@ -24,3 +41,12 @@ final class Day04: XCTestCase {
 
 
 // MARK: - Private
+private func _scan(_ string: String) -> [String] {
+    // !!!: implement me
+    return [ ]
+}
+
+private func _validCount(for lines: [String]) -> Int {
+    // !!!: impelement me
+    return 0
+}

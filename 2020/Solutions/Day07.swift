ab72c9826a784d6e43e1a3f4c9189749d17208b1
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-08 20:57:39 -0500

    day 7, 2020: Pass part 2 examples.

--- 2020/Solutions/Day07.swift
+++ 2020/Solutions/Day07.swift
@@ -27,7 +27,7 @@ final class Day07: XCTestCase {
         XCTAssertEqual(rule.ingredients, [ ])
     }
 
-    func test_example() {
+    func test_first_example() {
         let data = """
         light red bags contain 1 bright white bag, 2 muted yellow bags.
         dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -40,14 +40,43 @@ final class Day07: XCTestCase {
         dotted black bags contain no other bags.
         """
         let rules = data.components(separatedBy: .newlines).map { Rule($0) }
-        let containerMap = _parseContainerMap(from: rules)
-        XCTAssertEqual(_nestedContainers(for: "shiny gold", using: containerMap).count, 4)
+        
+        let parentMap = _parseParentMap(from: rules)
+        XCTAssertEqual(
+            _nestedContainers(for: "shiny gold", using: parentMap).count,
+            4
+        )
+        
+        let ingredientMap = _parseIngredientMap(from: rules)
+        XCTAssertEqual(
+            _nestedIngredientCount(for: "shiny gold", using: ingredientMap) - 1,
+            32
+        )
+    }
+
+    func test_second_example() {
+        let data = """
+        shiny gold bags contain 2 dark red bags.
+        dark red bags contain 2 dark orange bags.
+        dark orange bags contain 2 dark yellow bags.
+        dark yellow bags contain 2 dark green bags.
+        dark green bags contain 2 dark blue bags.
+        dark blue bags contain 2 dark violet bags.
+        dark violet bags contain no other bags.
+        """
+        let rules = data.components(separatedBy: .newlines).map { Rule($0) }
+        
+        let ingredientMap = _parseIngredientMap(from: rules)
+        XCTAssertEqual(
+            _nestedIngredientCount(for: "shiny gold", using: ingredientMap) - 1,
+            126
+        )
     }
     
     func test_solution() {
         let rules = Array(TestHarnessInput("input07.txt")!).map { Rule($0) }
-        let containerMap = _parseContainerMap(from: rules)
-        XCTAssertEqual(_nestedContainers(for: "shiny gold", using: containerMap).count, 242)
+        let parentMap = _parseParentMap(from: rules)
+        XCTAssertEqual(_nestedContainers(for: "shiny gold", using: parentMap).count, 242)
     }
     
 }
@@ -55,7 +84,8 @@ final class Day07: XCTestCase {
 
 // MARK: - Private
 private typealias Bag = String
-private typealias ContainerMap = [Bag: Set<Bag>]
+private typealias ParentMap = [Bag: Set<Bag>]
+private typealias IngredientMap = [Bag: [Ingredient]]
 
 private struct Ingredient: Hashable {
     let bag: Bag
@@ -91,21 +121,36 @@ private struct Rule {
     
 }
 
-private func _parseContainerMap(from rules: [Rule]) -> ContainerMap {
-    var result = ContainerMap()
+private func _parseParentMap(from rules: [Rule]) -> ParentMap {
+    var result = ParentMap()
     rules.forEach { rule in
         rule.ingredients.forEach { ingredient in
-            let containers = result[ingredient.bag] ?? Set<Bag>()
-            result[ingredient.bag] = containers.union(Set([rule.bag]))
+            let parents = result[ingredient.bag] ?? Set<Bag>()
+            result[ingredient.bag] = parents.union(Set([rule.bag]))
         }
     }
     return result
 }
 
-private func _nestedContainers(for bag: Bag, using containerMap: ContainerMap) -> Set<Bag> {
-    let containers = containerMap[bag] ?? Set<Bag>()
+private func _nestedContainers(for bag: Bag, using map: ParentMap) -> Set<Bag> {
+    let containers = map[bag] ?? Set<Bag>()
     let nestedContainers = containers.reduce(Set<Bag>()) { result, container in
-        result.union(_nestedContainers(for: container, using: containerMap))
+        result.union(_nestedContainers(for: container, using: map))
     }
     return containers.union(nestedContainers)
 }
+
+private func _parseIngredientMap(from rules: [Rule]) -> IngredientMap {
+    var result = IngredientMap()
+    rules.forEach { result[$0.bag] = $0.ingredients }
+    return result
+}
+
+private func _nestedIngredientCount(for bag: Bag, using map: IngredientMap) -> Int {
+    let ingredients = map[bag] ?? [ ]
+    
+    return 1 + ingredients.reduce(0) { result, ingredient in
+        result + ingredient.count * _nestedIngredientCount(for: ingredient.bag, using: map)
+    }
+}
+

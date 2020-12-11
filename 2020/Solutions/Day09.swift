f6b46ac7274b1976323d9fea243a601ae578caa0
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-10 20:14:18 -0500

    day 9, 2020: Sove first example.

--- 2020/Solutions/Day09.swift
+++ 2020/Solutions/Day09.swift
@@ -10,11 +10,19 @@
 //  Created by Otto Schnurr on 12/10/2020.
 //
 
+import Algorithms
 import XCTest
 
 final class Day09: XCTestCase {
 
     func test_example() {
+        let numbers = [
+            35, 20, 15, 25, 47,
+            40, 62, 55, 65, 95,
+            102, 117, 150, 182, 127,
+            219, 299, 277, 309, 576
+        ]
+        XCTAssertEqual(_firstCypherFailure(for: numbers, poolSize: 5)!, 127)
     }
 
     func test_solution() {
@@ -24,3 +32,18 @@ final class Day09: XCTestCase {
 
 
 // MARK: - Private
+func _firstCypherFailure(for numbers: [Int], poolSize: Int) -> Int? {
+    guard numbers.count > poolSize + 1 else { return nil }
+    
+    for index in (0 ..<  (numbers.count - poolSize)) {
+        let available = numbers[index ..< index + poolSize]
+            .combinations(ofCount: 2)
+            .map { $0.reduce(0, +) }
+        let availableValues = Set(available)
+        
+        let value = numbers[index + poolSize]
+        if !availableValues.contains(value) { return value }
+    }
+    
+    return nil
+}

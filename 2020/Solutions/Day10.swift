a9c760f6f10f59bf0d18aa4d2fa750c8aa5b2384
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-10 22:32:35 -0500

    day 10, 2020: Answer second example.

--- 2020/Solutions/Day10.swift
+++ 2020/Solutions/Day10.swift
@@ -15,13 +15,23 @@ import XCTest
 
 final class Day10: XCTestCase {
 
-    func test_example() {
-        let adapters = [ 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 ]
-        let differences = _differences(for: adapters)
-        let answer =
+    func test_examples() {
+        var adapters = [ 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 ]
+        var differences = _differences(for: adapters)
+        var answer =
             differences.filter { $0 == 1 }.count *
             (differences.filter { $0 == 3 }.count)
         XCTAssertEqual(answer, 35)
+        
+        adapters = [
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19,
+            38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3
+        ]
+        differences = _differences(for: adapters)
+        answer =
+            differences.filter { $0 == 1 }.count *
+            (differences.filter { $0 == 3 }.count)
+        XCTAssertEqual(answer, 220)
     }
 
     func test_solution() {

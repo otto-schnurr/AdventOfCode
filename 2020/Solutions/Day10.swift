1e5b29ad1c8b3cddeb0efcb181de1fc9cf75e4ea
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-10 22:05:33 -0500

    day 10, 2020: Add empty solution.
    Not yet implemented.

---
+++ 2020/Solutions/Day10.swift
@@ -0,0 +1,27 @@
+//  MIT License
+//  Copyright © 2020 Otto Schnurr
+//  https://github.com/otto-schnurr/AdventOfCode/blob/master/LICENSE
+
+//
+//  Day10.swift
+//  AdventOfCode-Solutions
+//
+//  A solution for https://adventofcode.com/2020/day/10
+//  Created by Otto Schnurr on 12/10/2020.
+//
+
+import Algorithms
+import XCTest
+
+final class Day10: XCTestCase {
+
+    func test_example() {
+    }
+
+    func test_solution() {
+    }
+    
+}
+
+
+// MARK: - Private

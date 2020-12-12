42846a568483ebbbb6b32f8a19bc0445184512f2
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-12 11:14:22 -0500

    day 12, 2020: Define Action and Instruction.

--- 2020/Solutions/Day12.swift
+++ 2020/Solutions/Day12.swift
@@ -16,6 +16,15 @@ import XCTest
 final class Day12: XCTestCase {
 
     func test_examples() {
+        let lines = """
+        F10
+        N3
+        F7
+        R90
+        F11
+        """.components(separatedBy: .newlines)
+        let instructions = lines.map { Insruction($0)! }
+        print(instructions)
     }
 
     func test_solution() {
@@ -25,3 +34,56 @@ final class Day12: XCTestCase {
 
 
 // MARK: - Private
+private typealias Position = SIMD2<Int>
+
+private enum Direction: Character {
+    case north = "N"
+    case south = "S"
+    case east = "E"
+    case west = "W"
+}
+
+private enum Movement: Character {
+    case left = "L"
+    case right = "R"
+    case forward = "F"
+}
+
+private enum Action {
+    
+    case direction(Direction)
+    case movement(Movement)
+    
+    init?(_ character: Character) {
+        if let direction = Direction(rawValue: character) {
+            self = .direction(direction)
+        } else if let movement = Movement(rawValue: character) {
+            self = .movement(movement)
+        } else {
+            return nil
+        }
+    }
+    
+}
+
+private struct Insruction {
+    
+    let action: Action
+    let value: Int
+    
+    init?(_ string: String) {
+        guard let firstCharacter = string.first else { return nil }
+        
+        let secondIndex = string.index(after: string.startIndex)
+        let suffix = string[secondIndex...]
+        
+        guard
+            let action = Action(firstCharacter),
+            let value = Int(suffix)
+        else { return nil }
+        
+        self.action = action
+        self.value = value
+    }
+    
+}

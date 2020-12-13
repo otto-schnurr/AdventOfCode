d390df73fcecd6a7ac2df08d2032465bb4554a7d
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-13 10:33:34 -0500

    day 13, 2020: Solve first example.

--- 2020/Solutions/Day13.swift
+++ 2020/Solutions/Day13.swift
@@ -16,6 +16,23 @@ import XCTest
 final class Day13: XCTestCase {
 
     func test_examples() {
+        let lines = """
+        939
+        7,13,x,x,59,x,31,19
+        """.components(separatedBy: .newlines)
+        let timestamp = Int(lines[0])!
+        let busIDs = lines[1]
+            .components(separatedBy: .punctuationCharacters)
+            .filter { $0 != "x" }
+            .compactMap { Int($0) }
+        let pairs = busIDs
+            .map { busID -> (busID: Int, departure: Int) in
+                let nextDeparture = (timestamp / busID + 1) * busID
+                return (busID: busID, departure: nextDeparture)
+            }
+        let bestPair = pairs
+            .min { $0.departure < $1.departure }!
+        XCTAssertEqual((bestPair.departure - 939) * bestPair.busID, 295)
     }
 
     func test_solution() {

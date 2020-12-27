7cee14d325252c2a68efe7e2800f91ea8f9ba1cf
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-27 15:47:14 -0500

    day 19, 2020: Add preliminary solution.
    Not yet implemented.

---
+++ 2020/Solutions/Day19.swift
@@ -0,0 +1,26 @@
+//  MIT License
+//  Copyright © 2020 Otto Schnurr
+//  https://github.com/otto-schnurr/AdventOfCode/blob/master/LICENSE
+
+//
+//  Day19.swift
+//  AdventOfCode-Solutions
+//
+//  A solution for https://adventofcode.com/2020/day/19
+//  Created by Otto Schnurr on 12/27/2020.
+//
+
+import XCTest
+
+final class Day19: XCTestCase {
+
+    func test_examples() {
+    }
+
+    func test_solution() {
+    }
+    
+}
+
+
+// MARK: - Private

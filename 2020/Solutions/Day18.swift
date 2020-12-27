8548c0e80947bebe94218e847fcbb78e3b44d831
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-26 21:32:33 -0500

    day 18, 2020: Solve second example.

--- 2020/Solutions/Day18.swift
+++ 2020/Solutions/Day18.swift
@@ -14,21 +14,25 @@ import XCTest
 
 final class Day18: XCTestCase {
 
-    func test_example_part1() {
+    func test_examples() {
         let lines = """
+        1 + (2 * 3) + (4 * (5 + 6))
         2 * 3 + (4 * 5)
         5 + (8 * 3 + 9 + 3 * 4 * 3)
         5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
         ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
         """.components(separatedBy: .newlines)
 
-        let results = lines.map { _evaluate($0) }
-        XCTAssertEqual(results, [ 26, 437, 12_240, 13_632 ])
+        var results = lines.map { _evaluate($0, precedence: .none) }
+        XCTAssertEqual(results, [ 51, 26, 437, 12_240, 13_632 ])
+
+        results = lines.map { _evaluate($0, precedence: .additionFirst) }
+        XCTAssertEqual(results, [ 51, 46, 1_445, 669_060, 23_340 ])
     }
 
     func test_solution() {
         let lines = Array(TestHarnessInput("input18.txt")!)
-        let results = lines.map { _evaluate($0) }
+        let results = lines.map { _evaluate($0, precedence: .none) }
         XCTAssertEqual(results.reduce(0, +), 4_940_631_886_147)
     }
     
@@ -36,12 +40,19 @@ final class Day18: XCTestCase {
 
 
 // MARK: - Private
-private func _evaluate(_ line: String) -> Int {
+private enum Precedence {
+    case none
+    case additionFirst
+}
+
+private func _evaluate(_ line: String, precedence: Precedence) -> Int {
     let expression = line.compactMap { Token(character: $0) }
-    let result = _simplify(expression)
-    switch result[0] {
-    case .value(let result): return result
-    default:                 return 0
+    let result = _simplify(expression, precedence: precedence)
+    
+    if case .value(let value) = result[0] {
+        return value
+    } else {
+        return 0
     }
 }
 
@@ -79,12 +90,25 @@ extension Token: CustomStringConvertible {
     }
 }
 
-private func _simplify(_ expression: [Token]) -> [Token] {
-    let firstPass = _reduceSubexpressions(in: expression)
-    return _reduceOperations(in: firstPass)
+private func _simplify(
+    _ expression: [Token], precedence: Precedence
+) -> [Token] {
+    let firstPass = _reduceSubexpressions(in: expression, precedence: precedence)
+    let secondPass: [Token]
+    
+    switch precedence {
+    case .none:
+        secondPass = firstPass
+    case .additionFirst:
+        secondPass = _reduceAdditions(in: firstPass)
+    }
+    
+    return _reduceOperations(in: secondPass)
 }
 
-private func _reduceSubexpressions(in expression: [Token]) -> [Token] {
+private func _reduceSubexpressions(
+    in expression: [Token], precedence: Precedence
+) -> [Token] {
     var subexpression = [Token]()
     
     return expression.reduce(into: [ ]) { result, token in
@@ -100,7 +124,7 @@ private func _reduceSubexpressions(in expression: [Token]) -> [Token] {
         }
         
         if !subexpression.isEmpty && subexpression.nestedCount == 0 {
-            result += _simplify(subexpression.dropFirst().dropLast())
+            result += _simplify(subexpression.dropFirst().dropLast(), precedence: precedence)
             subexpression = [ ]
         }
     }
@@ -112,13 +136,32 @@ private func _reduceOperations(in expression: [Token]) -> [Token] {
         let suffix = result.suffix(3)
         guard suffix.count == 3 else { return }
         
-        switch (suffix[0], suffix[1], suffix[2]) {
+        let end = result.endIndex
+        
+        switch (result[end - 3], result[end - 2], result[end - 1]) {
         case (.value(let lhs), .add, .value(let rhs)):
             result.removeLast(3)
             result.append(.value(lhs + rhs))
         case (.value(let lhs), .multiply, .value(let rhs)):
             result.removeLast(3)
             result.append(.value(lhs * rhs))
+        default:
+            break
+        }
+    }
+}
+
+private func _reduceAdditions(in expression: [Token]) -> [Token] {
+    return expression.reduce(into: [ ]) { result, token in
+        result.append(token)
+        guard result.count >= 3 else { return }
+        
+        let end = result.endIndex
+        
+        switch (result[end - 3], result[end - 2], result[end - 1]) {
+        case (.value(let lhs), .add, .value(let rhs)):
+            result.removeLast(3)
+            result.append(.value(lhs + rhs))
         default:
             break
         }

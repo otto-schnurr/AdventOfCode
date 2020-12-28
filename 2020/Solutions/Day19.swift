7868438d4f8dca979ca7166ccc306e2eb3c9797c
Otto Schnurr <otto.schnurr@gmail.com>
2020-12-27 20:54:56 -0500

    day 19, 2020: Set up preliminary methods.

--- 2020/Solutions/Day19.swift
+++ 2020/Solutions/Day19.swift
@@ -12,9 +12,29 @@
 
 import XCTest
 
+// reference: https://www.reddit.com/r/adventofcode/comments/kg1mro/2020_day_19_solutions/ggdxmpa/
 final class Day19: XCTestCase {
 
     func test_examples() {
+        let lines = """
+        0: 4 1 5
+        1: 2 3 | 3 2
+        2: 4 4 | 5 5
+        3: 4 5 | 5 4
+        4: "a"
+        5: "b"
+
+        ababbb
+        bababa
+        abbbab
+        aaabbb
+        aaaabbb
+        """.components(separatedBy: .newlines)
+        let (rules, messages) = _parse(lines)
+        let _ = messages.filter {
+            _validate(string: $0, with: rules, pending: [0])
+        }
+//        XCTAssertEqual(validMessages.count, 2)
     }
 
     func test_solution() {
@@ -24,3 +44,22 @@ final class Day19: XCTestCase {
 
 
 // MARK: - Private
+private typealias RuleID = Int
+private typealias Rules = [RuleID: Components]
+
+private enum Components {
+    case character(Character)
+    case subRules([[RuleID]])
+}
+
+private func _parse(_ lines: [String]) -> (rules: Rules, messages: [String]) {
+    // !!!: implement me
+    return ([:], [ ])
+}
+
+private func _validate(
+    string: String, with rules: Rules, pending: [RuleID]
+) -> Bool {
+    // !!!: implement me
+    return false
+}

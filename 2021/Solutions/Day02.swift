002148b9fa1d4823fea722ac678adb12b6798f1a
Otto Schnurr <otto.schnurr@gmail.com>
2021-12-03 10:55:54 -0500

    day 2, 2021: Add preliminary solution.
    Not yet implemented.

---
+++ 2021/Solutions/Day02.swift
@@ -0,0 +1,17 @@
+//  MIT License
+//  Copyright © 2021 Otto Schnurr
+//  https://github.com/otto-schnurr/AdventOfCode/blob/master/LICENSE
+
+//
+//  Day02.swift
+//  AdventOfCode/2021/Solutions
+//
+//  A solution for https://adventofcode.com/2021/day/2
+//  Created by Otto Schnurr on 12/3/2020.
+//
+
+import XCTest
+
+final class Day02: XCTestCase {
+
+}

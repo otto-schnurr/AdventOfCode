badc2eb8815bb0620007fe995bb797a57e234ef7
Otto Schnurr <otto.schnurr@gmail.com>
2021-12-02 22:49:42 -0500

    2021: Add preliminary test harness.

---
+++ 2021/Solutions/TestHarness.swift
@@ -0,0 +1,59 @@
+//  MIT License
+//  Copyright © 2021 Otto Schnurr
+//  https://github.com/otto-schnurr/AdventOfCode/blob/master/LICENSE
+
+//
+//  TestHarness.swift
+//  AdventOfCode-Solutions
+//
+//  Created by Otto Schnurr on 12/2/2021.
+//
+
+import Foundation
+import AdventOfCode
+
+extension URL {
+
+    init?(testHarnessResource: String) {
+        let resource = testHarnessResource as NSString
+        guard
+            let url = Bundle.module.url(
+                forResource: resource.deletingPathExtension,
+                withExtension: resource.pathExtension
+            )
+        else { return nil }
+
+        self = url
+    }
+
+}
+
+public struct TestHarnessInput: Sequence, IteratorProtocol {
+    
+    init?(_ resourceName: String, includeEmptyLines: Bool = false) {
+        guard
+            let fileURL = URL(testHarnessResource: resourceName),
+            let data = try? String(contentsOf: fileURL, encoding: .utf8)
+        else { return  nil }
+
+        self.includeEmptyLines = includeEmptyLines
+        lines = data.components(separatedBy: .newlines)
+        iterator = lines.makeIterator()
+    }
+    
+    public mutating func next() -> String? {
+        let result = iterator.next()
+        
+        if includeEmptyLines {
+            return result
+        } else {
+            return result?.isEmpty == true ? nil : result
+        }
+    }
+    
+    // MARK: Private
+    private let includeEmptyLines: Bool
+    private let lines: [String]
+    private var iterator: IndexingIterator<[String]>
+    
+}

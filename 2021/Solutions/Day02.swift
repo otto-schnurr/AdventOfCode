3ea0b8426d6f3fc9d6c38514783b765a621dabe7
Otto Schnurr <otto.schnurr@gmail.com>
2021-12-03 11:14:46 -0500

    day 2, 2021: Solve first example.

--- 2021/Solutions/Day02.swift
+++ 2021/Solutions/Day02.swift
@@ -14,4 +14,52 @@ import XCTest
 
 final class Day02: XCTestCase {
 
+    func test_example() {
+        let commands = [
+            "forward 5", "down 5", "forward 8",
+            "up 3",      "down 8", "forward 2"
+        ].compactMap { Command(text: $0) }
+        let result = commands.reduce(Position.zero) { previousPosition, command in
+            command.apply(to: previousPosition)
+        }
+        XCTAssertEqual(result.x * result.y, 150)
+    }
+
+}
+
+
+// MARK: - Privates
+private typealias Position = SIMD2<Int>
+
+private struct Command {
+    
+    let direction: String
+    let distance: Int
+    
+    init?(text: String) {
+        let components = text
+            .split(separator: " ")
+            .map { String($0) }
+        guard
+            components.count == 2,
+            let integer = Int(components[1])
+        else { return nil }
+
+        direction = components[0]
+        distance = integer
+    }
+    
+    func apply(to position: Position) -> Position {
+        var result = position
+
+        switch direction {
+        case "forward": result.x += distance
+        case "down":    result.y += distance
+        case "up":      result.y -= distance
+        default:        break
+        }
+        
+        return result
+    }
+    
 }

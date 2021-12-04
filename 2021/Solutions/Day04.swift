b004e024fe56c3becdb0781de4505d69e966bd4a
Otto Schnurr <otto.schnurr@gmail.com>
2021-12-04 15:40:52 -0500

    day 4, 2021: Add preliminary solution.

---
+++ 2021/Solutions/Day04.swift
@@ -0,0 +1,17 @@
+//  MIT License
+//  Copyright © 2021 Otto Schnurr
+//  https://github.com/otto-schnurr/AdventOfCode/blob/master/LICENSE
+
+//
+//  Day04.swift
+//  AdventOfCode/2021/Solutions
+//
+//  A solution for https://adventofcode.com/2021/day/4
+//  Created by Otto Schnurr on 12/4/2020.
+//
+
+import XCTest
+
+final class Day04: XCTestCase {
+
+}

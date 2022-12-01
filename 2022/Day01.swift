304b0b26de560b88fada47ad72e91d0c17d092ee
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-01 01:02:52 -0500

    day 1, 2022: Implement solution.

--- 2022/Day01.swift
+++ 2022/Day01.swift
@@ -6,22 +6,15 @@
 //  https://github.com/otto-schnurr/AdventOfCode/blob/main/LICENSE
 //  Copyright © 2022 Otto Schnurr
 
-import Algorithms // git@github.com:apple/swift-algorithms.git
-
-typealias Signal = [Int]
-
-var scan = Signal()
-while let line = readLine(), let depth = Int(line) { scan.append(depth) }
-
-func accumulate(_ scan: Signal, windowSize: Int) -> Signal {
-    return scan
-        .windows(ofCount: windowSize)
-        .map { $0.reduce(0, +) }
+struct Input: Sequence, IteratorProtocol {
+    func next() -> String? { return readLine() }
 }
 
-func incrementCount(for signal: Signal) -> Int {
-    return signal.adjacentPairs().filter { $0 < $1 }.count
-}
+let inventory = Input()
+    .map { Int($0) }
+    .split(separator: nil)
+    .map { $0.compactMap { $0 }.reduce(0, +) }
+    .sorted(by: >)
 
-print(incrementCount(for: accumulate(scan, windowSize: 1)))
-print(incrementCount(for: accumulate(scan, windowSize: 3)))
+print("top one   : \(inventory.first ?? 0)")
+print("top three : \(inventory.prefix(3).reduce(0, +))")

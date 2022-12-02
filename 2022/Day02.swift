cfc79bebe3d3bdf7453ec4979f37408765d904fb
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-02 08:35:40 -0500

    day 2, 2022: Solve part 1.

--- 2022/Day02.swift
+++ 2022/Day02.swift
@@ -6,6 +6,19 @@
 //  https://github.com/otto-schnurr/AdventOfCode/blob/main/LICENSE
 //  Copyright © 2022 Otto Schnurr
 
+let scoreForRound: [String: Int] = [
+    "A X": 1 + 3, "A Y": 2 + 6, "A Z": 3 + 0,
+    "B X": 1 + 0, "B Y": 2 + 3, "B Z": 3 + 6,
+    "C X": 1 + 6, "C Y": 2 + 0, "C Z": 3 + 3
+]
+
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
+
+let score = StandardInput()
+    .compactMap { $0 }
+    .map { scoreForRound[$0] ?? 0 }
+    .reduce(0, +)
+
+print("score : \(score)")

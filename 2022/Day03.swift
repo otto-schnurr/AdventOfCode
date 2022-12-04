da527984bcd112755c896b4f6236695d0b04529a
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-03 21:32:09 -0500

    day 3, 2022: Preliminary solution.
    Not yet implemented.

---
+++ 2022/Day03.swift
@@ -0,0 +1,11 @@
+#!/usr/bin/env swift sh
+
+//  A solution for https://adventofcode.com/2022/day/3
+//
+//  MIT License
+//  https://github.com/otto-schnurr/AdventOfCode/blob/main/LICENSE
+//  Copyright © 2022 Otto Schnurr
+
+struct StandardInput: Sequence, IteratorProtocol {
+    func next() -> String? { return readLine() }
+}

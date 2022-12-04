0474fc32fa12bc77d591cf03e3e2cece4f927ca5
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-03 22:39:45 -0500

    day 3, 2022: Solve part 1.

--- 2022/Day03.swift
+++ 2022/Day03.swift
@@ -9,3 +9,23 @@
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
+
+extension Character {
+    var priority: Int? {
+        if isLowercase {
+            return Int(asciiValue! - Character("a").asciiValue! + 1)
+        } else {
+            return Int(asciiValue! - Character("A").asciiValue! + 27)
+        }
+    }
+}
+
+let priorities = StandardInput()
+    .compactMap { $0 }
+    .compactMap {
+        let middle = $0.count / 2
+        return Set($0.prefix(middle)).intersection($0.suffix(middle)).first
+    }.compactMap {
+        Character(String($0)).priority
+    }
+print(priorities.reduce(0, +))

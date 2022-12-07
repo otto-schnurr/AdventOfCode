e7e96e88406bfeca422bca7453943df68d767a48
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-06 20:01:30 -0500

    day 6, 2022: Simplify function definitions.

--- 2022/Day06.swift
+++ 2022/Day06.swift
@@ -11,16 +11,15 @@ import Algorithms // https://github.com/apple/swift-algorithms
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
+
+func markerEnd(for signal: String, markerLength: Int) -> Int {
+    return Array(signal.windows(ofCount: markerLength))
+        .firstIndex { Set($0).count == markerLength }! + markerLength
+}
+
 let signals = StandardInput().compactMap { $0 }
 let part1 = signals.map { markerEnd(for: $0, markerLength: 4)}.reduce(0, +)
 let part2 = signals.map { markerEnd(for: $0, markerLength: 14)}.reduce(0, +)
 
 print("part 1 : \(part1)")
 print("part 2 : \(part2)")
-
-
-// MARK: - Private
-private func markerEnd(for signal: String, markerLength: Int) -> Int {
-    return Array(signal.windows(ofCount: markerLength))
-        .firstIndex { Set($0).count == markerLength }! + markerLength
-}

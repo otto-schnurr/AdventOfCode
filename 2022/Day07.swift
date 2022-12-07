5face35608fa074e349ee2b223215e939da47422
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-07 18:32:37 -0500

    day 7, 2022: Solve part 1.

--- 2022/Day07.swift
+++ 2022/Day07.swift
@@ -9,3 +9,28 @@
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
+
+var part1 = 0
+
+func processLines(_ iterator: StandardInput.Iterator) -> Int {
+    var result = 0
+    defer { if result <= 100_000 { part1 += result } }
+
+    while let nextLine = iterator.next() {
+        let split = nextLine.split(separator: " ")
+        
+        if let storage = Int(split[0]) {
+            result += storage
+        } else if nextLine == "$ cd .." {
+            return result
+        } else if split[1] == "cd" {
+            result += processLines(iterator)
+        }
+    }
+
+    return result
+}
+
+let lines = StandardInput()
+_ = processLines(StandardInput().makeIterator())
+print(part1)

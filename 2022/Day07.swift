83a33dd884c08007bde4917168037a8d7ea88e7c
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-07 20:54:22 -0500

    day 7, 2022: Refactor using closure.

--- 2022/Day07.swift
+++ 2022/Day07.swift
@@ -6,46 +6,49 @@
 //  https://github.com/otto-schnurr/AdventOfCode/blob/main/LICENSE
 //  Copyright © 2022 Otto Schnurr
 
-var threshold = 0
-var part1 = 0
-var part2 = 0
-
-func processLines(_ iterator: inout Array<String>.Iterator) -> Int {
-    var storage = 0
-    defer {
-        if storage <= 100_000 { part1 += storage }
-        if storage >= threshold && storage < part2 { part2 = storage }
-    }
-
-    while let nextLine = iterator.next() {
-        let split = nextLine.split(separator: " ")
-        
-        if let fileStorage = Int(split[0]) {
-            storage += fileStorage
-        } else if nextLine == "$ cd .." {
-            return storage
-        } else if split[1] == "cd" {
-            storage += processLines(&iterator)
-        }
-    }
-
-    return storage
-}
-
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
 
-var lines = Array(StandardInput())
-var iterator = lines.makeIterator()
-let usedSpace = processLines(&iterator)
+func process(
+    remainingLines: inout Array<String>.Iterator,
+    hook: (_ subdirectorySize: Int) -> Void
+) -> Int {
+    var subdirectorySize = 0
+    defer { hook(subdirectorySize) }
+    
+    while let nextLine = remainingLines.next() {
+        let word = nextLine.split(separator: " ")
+        
+        if let fileSize = Int(word[0]) {
+            subdirectorySize += fileSize
+        } else if nextLine == "$ cd .." {
+            return subdirectorySize
+        } else if word[1] == "cd" {
+            subdirectorySize += process(
+                remainingLines: &remainingLines, hook: hook
+            )
+        }
+    }
 
-threshold = usedSpace - 40_000_000
-part1 = 0
-part2 = Int.max
+    return subdirectorySize
+}
+
+let lines = Array(StandardInput())
+var iterator = lines.makeIterator()
+var part1 = 0
+
+let usedSpace = process(remainingLines: &iterator) {
+    if $0 <= 100_000 { part1 += $0 }
+}
 
 iterator = lines.makeIterator()
-_ = processLines(&iterator)
+let desiredSize = usedSpace - 40_000_000
+var part2 = Int.max
+
+_ = process(remainingLines: &iterator) {
+    if $0 >= desiredSize && $0 < part2 { part2 = $0 }
+}
 
 print("part 1 : \(part1)")
 print("part 2 : \(part2)")

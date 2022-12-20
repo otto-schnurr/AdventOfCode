330e798544a2103ee1c8ea7a58484735fe0bcf81
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-20 13:13:02 -0500

    day 20, 2022: Preliminary array extension.
    In hopes of not having to move memory for the majority of the array.
    
    This solution is currently incomplete.

--- 2022/Day20.swift
+++ 2022/Day20.swift
@@ -10,6 +10,32 @@ struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
 
-let list = StandardInput().compactMap(Int.init)
+extension RangeReplaceableCollection where Index == Int {
+    mutating func moveElement(from sourceIndex: Index, distance: Index) {
+        guard !isEmpty else { return }
+        
+        let source = sourceIndex % count
+        let _target = (source + distance) % count
+        let target = _target < 0 ? count + _target : _target
+        let element = self[source]
+        
+        if source < target {
+            replaceSubrange(source ..< target, with: self[source + 1 ... target])
+        } else if target < source {
+            replaceSubrange(target + 1 ... source, with: self[target ..< source])
+        }
 
-print("list count: \(list.count), unique count: \(Set(list).count)")
+        replaceSubrange(target ... target, with: [element])
+    }
+}
+
+var list = Array(StandardInput().compactMap(Int.init).enumerated())
+print(list)
+
+for originalIndex in 0..<list.count {
+    let source = list.firstIndex { $0.offset == originalIndex }!
+    list.moveElement(from: source, distance: list[source].element)
+    print(list)
+}
+
+print(list)

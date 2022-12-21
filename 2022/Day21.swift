fad92aa3f1b4257f5a3f77565e5f74972600b23e
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-21 11:15:31 -0500

    day 21, 2022: Parse input.

--- 2022/Day21.swift
+++ 2022/Day21.swift
@@ -9,3 +9,25 @@
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
+
+typealias Name = String
+typealias Expression = (lhs: String, operation: String, rhs: String)
+
+var valueFor = [Name: Int]()
+var expressionFor = [Name: Expression]()
+
+StandardInput().forEach { line in
+    let words = line.split(separator: " ").map(String.init)
+    let name = String(words[0].prefix(words[0].count - 1))
+
+    if let value = Int(words[1]) {
+        valueFor[name] = value
+    } else {
+        expressionFor[name] = Expression(
+            lhs: words[1], operation: words[2], rhs: words[3]
+        )
+    }
+}
+
+print(valueFor)
+print(expressionFor)

8befa48476b17be749890b0d206eeaad24ef13d4
Otto Schnurr <otto.schnurr@gmail.com>
2022-12-24 16:59:03 -0500

    day 14, 2022: Implement parse.

--- 2022/Day14.swift
+++ 2022/Day14.swift
@@ -9,3 +9,21 @@
 struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
+
+typealias Position = SIMD2<Int>
+typealias Segment = (from: Position, to: Position)
+
+struct Grid {
+    private(set) var positions: Set<Position>
+}
+
+let segments = StandardInput().map { line in
+    let words = Array(line.split(separator: " "))
+    let positions = stride(from: 0, through: words.count, by: 2).map {
+        let numbers = words[$0].split(separator: ",").map { Int($0)! }
+        return Position(numbers[0], numbers[1])
+    }
+    return zip(positions, positions[1...]).map { from, to in Segment(from, to) }
+}
+
+print(segments)

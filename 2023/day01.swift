91f939b8ae9c588bec645257144e7fe4f7508870
Otto Schnurr <otto.schnurr@gmail.com>
2023-12-05 18:29:32 -0500

    day 1, 2023: Preliminary solution.
    Not yet implemented.

---
+++ 2023/day01.swift
@@ -0,0 +1,9 @@
+#!/usr/bin/env swift sh
+
+//  A solution for https://adventofcode.com/2023/day/1
+//
+//  MIT License
+//  https://github.com/otto-schnurr/AdventOfCode/blob/main/LICENSE
+//  Copyright © 2023 Otto Schnurr
+
+print("Hello world.")

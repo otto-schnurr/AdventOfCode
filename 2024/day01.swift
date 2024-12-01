1f5ce8e11bc9472342b4f25b1d906ba144aa6838
Otto Schnurr <otto.schnurr@gmail.com>
2024-12-01 16:44:49 -0500

    day 1, 2024: Solve part 1.

--- 2024/day01.swift
+++ 2024/day01.swift
@@ -6,5 +6,17 @@
 //  https://github.com/otto-schnurr/AdventOfCode/blob/main/LICENSE
 //  Copyright © 2024 Otto Schnurr
 
-print("Hello world.")
+struct StandardInput: Sequence, IteratorProtocol {
+    func next() -> String? { return readLine() }
+}
 
+let locationIDs = StandardInput().map { pair in
+    pair.split(separator: " ").map { Int($0)! }
+}
+let unzipped = (
+    lhs: locationIDs.map { $0[0] }.sorted(),
+    rhs: locationIDs.map { $0[1] }.sorted()
+)
+let distances = zip(unzipped.lhs, unzipped.rhs).map { abs($0.0 - $0.1) }
+
+print("part 1 : \(distances.reduce(0, +))")

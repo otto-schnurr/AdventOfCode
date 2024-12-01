57bc44ff3fab1738f52d294e92021d9fc3a678ac
Otto Schnurr <otto.schnurr@gmail.com>
2024-12-01 17:09:03 -0500

    day 1, 2024: Simplify.

--- 2024/day01.swift
+++ 2024/day01.swift
@@ -13,18 +13,17 @@ struct StandardInput: Sequence, IteratorProtocol {
 let locationIDs = StandardInput().map { pair in
     pair.split(separator: " ").map { Int($0)! }
 }
-let unzipped = (
-    lhs: locationIDs.map { $0[0] }.sorted(),
-    rhs: locationIDs.map { $0[1] }.sorted()
-)
-let distances = zip(unzipped.lhs, unzipped.rhs).map { abs($0.0 - $0.1) }
+
+let lhs = locationIDs.map { $0[0] }.sorted()
+let rhs = locationIDs.map { $0[1] }.sorted()
+let distances = zip(lhs, rhs).map { abs($0.0 - $0.1) }
 print("part 1 : \(distances.reduce(0, +))")
 
 var countForLocation = [Int: Int]()
 
-for locationID in unzipped.rhs {
+for locationID in rhs {
     countForLocation[locationID, default: 0] += 1
 }
 
-let similarities = unzipped.lhs.map { $0 * countForLocation[$0, default: 0] }
+let similarities = lhs.map { $0 * countForLocation[$0, default: 0] }
 print("part 2 : \(similarities.reduce(0, +))")

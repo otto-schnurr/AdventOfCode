caba9db2302c82d4699abdb9e931d6cb37cae655
Otto Schnurr <otto.schnurr@gmail.com>
2024-12-03 21:45:27 -0500

    day 2, 2024: Solve part 2.

--- 2024/day02.swift
+++ 2024/day02.swift
@@ -12,14 +12,40 @@ struct StandardInput: Sequence, IteratorProtocol {
     func next() -> String? { return readLine() }
 }
 
-func isSafe(_ deltas: [Int])  -> Bool  {
-    deltas.allSatisfy { (-3 ... -1).contains($0) } ||
-    deltas.allSatisfy { (+1 ... +3).contains($0) }
+typealias Record = [Int]
+let safelyIncreasing = +1 ... +3
+let safelyDecreasing = -3 ... -1
+
+let records = StandardInput().map { record in
+    record.split(separator: " ").map { Int($0)! }
+}
+print("part 1 : \(records.filter(isSafe).count)")
+print("part 2 : \(records.filter(dampen).count)")
+
+func isSafe(_ record: Record)  -> Bool  {
+    let deltas = record.adjacentPairs().map { $1 - $0 }
+    return
+        deltas.allSatisfy { safelyIncreasing.contains($0) } ||
+        deltas.allSatisfy { safelyDecreasing.contains($0) }
 }
 
-let deltas = StandardInput().map { record in
-    record
-        .split(separator: " ").map { Int($0)! }
-        .adjacentPairs().map { $1 - $0 }
+func dampen(record: Record) -> Bool {
+    return
+        prune(record, with: safelyIncreasing) ||
+        prune(record, with: safelyDecreasing)
 }
-print("part 1 : \(deltas.filter(isSafe).count)")
+
+func prune(_ record: Record, with range: ClosedRange<Int>) -> Bool {
+    var result = isSafe(record)
+    let deltas = record.adjacentPairs().map { $1 - $0 }
+
+    if let index = deltas.firstIndex(where: { !range.contains($0) }) {
+        result = result || (index ... index + 1).contains {
+            var pruned = record
+            pruned.remove(at: $0)
+            return isSafe(pruned)
+        }
+    }
+
+    return result
+}

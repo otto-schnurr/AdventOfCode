716645d4fbec7b0ced465182ca25f9a286ff9398
Otto Schnurr <otto.schnurr@gmail.com>
2025-12-16 10:41:19 -0500

    day 1: Add preliminary _parseNumber().
    Not yet implemented.

--- 2025/day01.cpp
+++ 2025/day01.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using std::replace;
 
+namespace
+{
+    std::optional<int> _parseNumber( std::string line )
+    {
+        // implement me
+        (void)line;
+        return 42;
+    }
+}
+
 int main()
 {
     for ( std::string line; std::getline( std::cin, line ); )
     {
         replace( line.begin(), line.end(), 'L', '-' );
         replace( line.begin(), line.end(), 'R', '+' );
-        std::cout << line << std::endl;
+
+        if ( auto number = _parseNumber( line ) )
+        {
+            std::cout << *number << std::endl;
+        }
     }
 
     return EXIT_SUCCESS;

fb4fc32605a32a3c22d08fb4068bb3f3fb6199d6
Otto Schnurr <otto.schnurr@gmail.com>
2025-12-16 17:14:59 -0500

    day 1: Solve part 1.

--- 2025/day01.cpp
+++ 2025/day01.cpp
@@ -20,15 +20,31 @@ namespace
         std::from_chars( line.data(), line.data() + line.size(), result );
         return result;
     }
+
+    int _rotateDial( int dial, int amount )
+    {
+        const int dialSize = 100;
+
+        dial += amount % dialSize;
+        dial %= dialSize;
+        dial += dial < 0 ? dialSize : 0;
+        
+        return dial;
+    }
 }
 
 int main()
 {
+    int dial = 50;
+    int zeroCount = 0;
+
     for ( std::string line; std::getline( std::cin, line ); )
     {
-        const int number = _parseNumber( line );
-        std::cout << number << std::endl;
+        const int amount = _parseNumber( line );
+        dial = _rotateDial( dial, amount );
+        zeroCount += dial == 0 ? 1 : 0;
     }
 
+    std::cout << "part 1: " << zeroCount << std::endl;
     return EXIT_SUCCESS;
 }

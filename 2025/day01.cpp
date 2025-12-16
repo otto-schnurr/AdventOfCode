a311aa3852f0e3cf368751bf3b90b8229148ba1b
Otto Schnurr <otto.schnurr@gmail.com>
2025-12-15 23:07:30 -0500

    day 1: Convert L/R to -/+.

--- 2025/day01.cpp
+++ 2025/day01.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+using std::replace;
 
 int main()
 {
     for ( std::string line; std::getline( std::cin, line ); )
     {
+        replace( line.begin(), line.end(), 'L', '-' );
+        replace( line.begin(), line.end(), 'R', '+' );
         std::cout << line << std::endl;
     }
 

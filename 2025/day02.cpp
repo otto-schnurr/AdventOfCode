130cc1f372756d74498db806c7b955ebdaf97c25
Otto Schnurr <otto.schnurr@gmail.com>
2025-12-17 20:56:39 -0500

    day 2: Parse ranges.

---
+++ 2025/day02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    for ( std::string line; std::getline( std::cin, line, ',' ); )
+    {
+        std::cout << line << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}

e7aca694b9ce48a886e5affe8b744448ed63eb47
Otto Schnurr <otto.schnurr@gmail.com>
2025-12-18 00:10:50 -0500

    day 2: First attempt at solving part 1.
    known issue: The solution is not yet correct.

--- 2025/day02.cpp
+++ 2025/day02.cpp
@@ -1,5 +1,5 @@
-#include <charconv>
 #include <iostream>
+#include <charconv>
 #include <regex>
 
 using Range = std::pair<int, int>;
@@ -23,16 +23,48 @@ namespace
 
         return Range( _parseNumber( *iWord++ ), _parseNumber( *iWord ) );
     }
+
+    bool _isInvalid( int id )
+    {
+        bool result = false;
+        const int digitCount = static_cast<int>( log10( id ) ) + 1;
+        const bool evenNumberOfDigits = digitCount % 2 == 0;
+
+        if ( evenNumberOfDigits )
+        {
+            const int halfLength = digitCount / 2;
+            const int divisor = static_cast<int>( pow( 10, halfLength ) );
+
+            const int firstHalf = id / divisor;
+            const int secondHalf = id % divisor;
+            result = firstHalf == secondHalf;
+        }
+
+        return result;
+    }
+
+    int _invalidSum( Range range )
+    {
+        int result = 0;
+
+        for ( auto id = range.first; id <= range.second; ++id )
+        {
+            result += _isInvalid( id ) ? id : 0;
+        }
+
+        return result;
+    }
 }
 
 int main()
 {
+    int part1 = 0;
+
     for ( std::string line; std::getline( std::cin, line, ',' ); )
     {
-        const Range range = _parseRange( line );
-        std::cout << "from " << range.first;
-        std::cout << " to " << range.second << std::endl;
+        part1 += _invalidSum( _parseRange( line ) );
     }
 
+    std::cout << "part 1: " << part1 << std::endl;
     return EXIT_SUCCESS;
 }

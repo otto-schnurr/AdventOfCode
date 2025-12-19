c405a29ab278e380adbecb2deef69c43990dc403
Otto Schnurr <otto.schnurr@gmail.com>
2025-12-19 09:55:00 -0500

    day 3: Define Joltage type.
    In anticipation of needing an integer wider than int.

--- 2025/day03.cpp
+++ 2025/day03.cpp
@@ -2,15 +2,17 @@
 #include <algorithm>
 #include <charconv>
 
+using Joltage = long;
+
 namespace
 {
-    int _processLine( const std::string& line )
+    Joltage _processLine( const std::string& line )
     {
         const auto firstDigit = max_element( line.begin(), line.end() - 1 );
         const auto secondDigit = max_element( firstDigit + 1, line.end() );
         const auto word = std::string() + *firstDigit + *secondDigit;
 
-        int result = 0;
+        Joltage result = 0;
         std::from_chars( word.data(), word.data() + word.length(), result );
         return result;
     }
@@ -18,8 +20,8 @@ namespace
 
 int main()
 {
+    Joltage part1 = 0;
     std::string line;
-    int part1 = 0;
 
     while ( std::getline( std::cin, line ) )
     {
